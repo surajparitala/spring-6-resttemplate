ebea800eaaf98e4b981bbe689c0b3393b81bdccb
surajparitala <paritala.suraj@gmail.com>
2023-03-17 00:12:51 -0400

    getting map list response instead of string

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 /**
  * @author surajparitala
  * Date: 3/16/23
@@ -16,13 +18,20 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class BeerClientImpl implements BeerClient {
     private final RestTemplateBuilder restTemplateBuilder;
+
+    private static final String BASE_URL="http://localhost:8080";
+    private static final String GET_BEER_PATH="/api/v1/beer";
+
     @Override
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate= restTemplateBuilder.build();
 
-        ResponseEntity<String> stringResponse =
-                restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
-        System.out.println(stringResponse.getBody());
+//        ResponseEntity<String> stringResponse =
+//                restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
+
+        ResponseEntity<Map> mapResponseEntity =
+                restTemplate.getForEntity(BASE_URL+GET_BEER_PATH, Map.class);
+        System.out.println(mapResponseEntity.getBody());
         return null;
     }
 }
