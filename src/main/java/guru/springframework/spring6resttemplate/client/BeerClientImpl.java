021ca74e97252309eee84e93693f0d02e03f7f7b
surajparitala <paritala.suraj@gmail.com>
2023-03-17 00:07:26 -0400

    get list beers impl

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -1,15 +1,28 @@
 package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author surajparitala
  * Date: 3/16/23
  */
+@Service
+@RequiredArgsConstructor
 public class BeerClientImpl implements BeerClient {
+    private final RestTemplateBuilder restTemplateBuilder;
     @Override
     public Page<BeerDTO> listBeers() {
+        RestTemplate restTemplate= restTemplateBuilder.build();
+
+        ResponseEntity<String> stringResponse =
+                restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
+        System.out.println(stringResponse.getBody());
         return null;
     }
 }
--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,6 +1,8 @@
 package guru.springframework.spring6resttemplate.client;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,9 +10,13 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author surajparitala
  * Date: 3/16/23
  */
+@SpringBootTest
 class BeerClientImplTest {
+
+    @Autowired
+    BeerClientImpl beerClient;
     @Test
     void listBeers() {
-
+        beerClient.listBeers();
     }
 }
