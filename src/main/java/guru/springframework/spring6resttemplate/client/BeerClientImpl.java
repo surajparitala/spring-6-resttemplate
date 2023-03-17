a9ccf1457f344c7eaeac95159e5e006909890d37
surajparitala <paritala.suraj@gmail.com>
2023-03-17 00:34:24 -0400

    beer dto page impl to use deserialization feature

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -1,15 +1,13 @@
 package guru.springframework.spring6resttemplate.client;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import guru.springframework.spring6resttemplate.model.BeerDTOPageImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Map;
 
 /**
  * @author surajparitala
@@ -30,16 +28,8 @@ public class BeerClientImpl implements BeerClient {
 //        ResponseEntity<String> stringResponse =
 //                restTemplate.getForEntity("http://localhost:8080/api/v1/beer", String.class);
 
-        ResponseEntity<Map> mapResponseEntity =
-                restTemplate.getForEntity(BASE_URL+GET_BEER_PATH, Map.class);
-
-        ResponseEntity<JsonNode> jsonResponseEntity =
-                restTemplate.getForEntity(BASE_URL+GET_BEER_PATH, JsonNode.class);
-
-        jsonResponseEntity.getBody().findPath("content").elements().forEachRemaining(jsonNode ->
-                System.out.println(jsonNode.get("beerName").asText()));
-
-        System.out.println(mapResponseEntity.getBody());
+            ResponseEntity<BeerDTOPageImpl> pageResponseEntity=
+                    restTemplate.getForEntity(BASE_URL+GET_BEER_PATH, BeerDTOPageImpl.class);
         return null;
     }
 }
