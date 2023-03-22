4a317624a88e7a0215845ab1d05e60943b82f357
surajparitala <paritala.suraj@gmail.com>
2023-03-22 15:36:36 -0400

    test splice for rest template client and using custom rest template and builder

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -22,8 +22,8 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class BeerClientImpl implements BeerClient {
     private final RestTemplateBuilder restTemplateBuilder;
-    private static final String GET_BEER_PATH="/api/v1/beer";
-    private static final String GET_BEER_BY_ID="/api/v1/beer/{beerId}";
+    public static final String GET_BEER_PATH="/api/v1/beer";
+    public static final String GET_BEER_BY_ID="/api/v1/beer/{beerId}";
 
     @Override
     public Page<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory, Integer pageNumber,
---
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientTest.java
@@ -0,0 +1,84 @@
+package guru.springframework.spring6resttemplate.client;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.spring6resttemplate.config.RestTemplateBuilderConfig;
+import guru.springframework.spring6resttemplate.model.BeerDTO;
+import guru.springframework.spring6resttemplate.model.BeerDTOPageImpl;
+import guru.springframework.spring6resttemplate.model.BeerStyle;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.boot.test.web.client.MockServerRestTemplateCustomizer;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+
+/**
+ * @author surajparitala
+ * Date: 3/22/23
+ */
+@RestClientTest(BeerClientImpl.class)
+@Import(RestTemplateBuilderConfig.class)
+class BeerClientTest {
+    BeerClient beerClient;
+    MockRestServiceServer server;
+    @Autowired
+    RestTemplateBuilder restTemplateBuilderConfigured;
+    @Autowired
+    ObjectMapper objectMapper;
+    @Mock
+    RestTemplateBuilder mockRestTemplateBuilder= new RestTemplateBuilder(new MockServerRestTemplateCustomizer());
+
+    static final String URL= "http://localhost:8080";
+
+    @BeforeEach
+    void setUp() {
+        RestTemplate restTemplate= restTemplateBuilderConfigured.build();
+
+        server = MockRestServiceServer.bindTo(restTemplate).build();
+        when(mockRestTemplateBuilder.build()).thenReturn(restTemplate);
+        beerClient = new BeerClientImpl(mockRestTemplateBuilder);
+    }
+
+    @Test
+    void testListBeers() throws JsonProcessingException {
+        String payload = objectMapper.writeValueAsString(getPage());
+
+        server.expect(method(HttpMethod.GET)).andExpect(requestTo(URL+BeerClientImpl.GET_BEER_PATH))
+                .andRespond(withSuccess(payload, MediaType.APPLICATION_JSON));
+
+        Page<BeerDTO> beerDTOS = beerClient.listBeers();
+        assertThat(beerDTOS.getContent().size()).isPositive();
+    }
+
+    BeerDTO getBeerDTO() {
+        return BeerDTO.builder()
+                .price(BigDecimal.valueOf(10.99))
+                .beerName("Mango")
+                .beerStyle(BeerStyle.IPA)
+                .quantityOnHand(500)
+                .upc("12345")
+                .build();
+    }
+
+    BeerDTOPageImpl getPage() {
+        return new BeerDTOPageImpl(Collections.singletonList(getBeerDTO()),1,25,1);
+    }
+}
