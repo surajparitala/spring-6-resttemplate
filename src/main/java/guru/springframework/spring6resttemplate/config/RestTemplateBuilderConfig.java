6ca5ca7deb3cea59e2dbd2ba65e16cd39ba236bf
surajparitala <paritala.suraj@gmail.com>
2023-03-24 17:41:11 -0400

    adding client manager

--- src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
+++ src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
@@ -1,10 +1,13 @@
 package guru.springframework.spring6resttemplate.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.client.RestTemplateBuilderConfigurer;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.*;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 /**
@@ -12,10 +15,26 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
  * Date: 3/17/23
  */
 @Configuration
+@RequiredArgsConstructor
 public class RestTemplateBuilderConfig {
 
     @Value("${rest.template.rootUrl}")
     String rootUrl;
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
+    private final OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+
+    @Bean
+    OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager(){
+        OAuth2AuthorizedClientProvider authorizedClientProvider
+                = OAuth2AuthorizedClientProviderBuilder.builder().clientCredentials().build();
+
+        AuthorizedClientServiceOAuth2AuthorizedClientManager authorizedClientServiceOAuth2AuthorizedClientManager
+                = new AuthorizedClientServiceOAuth2AuthorizedClientManager(clientRegistrationRepository, oAuth2AuthorizedClientService);
+
+        authorizedClientServiceOAuth2AuthorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+        return authorizedClientServiceOAuth2AuthorizedClientManager;
+    }
     @Bean
     RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer) {
         assert rootUrl != null;
