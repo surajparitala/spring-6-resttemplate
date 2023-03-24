a805c381d560c51a538a021ed015945b8be32375
surajparitala <paritala.suraj@gmail.com>
2023-03-24 17:04:11 -0400

    updating app props for jwt client and removing basic auth

--- src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
+++ src/main/java/guru/springframework/spring6resttemplate/config/RestTemplateBuilderConfig.java
@@ -16,16 +16,11 @@ public class RestTemplateBuilderConfig {
 
     @Value("${rest.template.rootUrl}")
     String rootUrl;
-    @Value("${rest.template.username}")
-    String username;
-    @Value("${rest.template.password}")
-    String password;
     @Bean
     RestTemplateBuilder restTemplateBuilder(RestTemplateBuilderConfigurer configurer) {
         assert rootUrl != null;
 
         return configurer.configure(new RestTemplateBuilder())
-                .basicAuthentication(username, password)
                 .uriTemplateHandler(new DefaultUriBuilderFactory(rootUrl));
     }
 }
